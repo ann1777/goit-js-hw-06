29fa835cec99da37d72dd92eff1890019c274ad6
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-15 13:43:51 +0300

    task-06.js completed

--- js/task-06.js
+++ js/task-06.js
@@ -20,3 +20,16 @@
 //  border-color: #f44336;
 // }
 
+const input = document.getElementById('validation-input');
+
+input.addEventListener('blur', () => {
+  const expectedLength = input.getAttribute('data-length');
+  const actualLength = input.value.length;
+  if (actualLength === parseInt(expectedLength)) {
+    input.classList.remove('invalid');
+    input.classList.add('valid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  }
+});
