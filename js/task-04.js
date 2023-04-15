e2f0e98c4aa4a49139046bb3adce993257e2a593
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-15 13:33:22 +0300

    task-04.js completed

--- js/task-04.js
+++ js/task-04.js
@@ -3,3 +3,27 @@
 // Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
 // Оновлюй інтерфейс новим значенням змінної counterValue.
 
+// Get references to the relevant DOM elements
+const counterEl = document.getElementById('counter');
+const valueEl = document.getElementById('value');
+const decrementBtn = counterEl.querySelector('[data-action="decrement"]');
+const incrementBtn = counterEl.querySelector('[data-action="increment"]');
+
+// Initialize the counter value to zero
+let counterValue = 0;
+
+// Add click listeners to the buttons
+decrementBtn.addEventListener('click', () => {
+  counterValue -= 1;
+  updateCounterValue();
+});
+
+incrementBtn.addEventListener('click', () => {
+  counterValue += 1;
+  updateCounterValue();
+});
+
+// Update the interface with the new value of the counterValue variable
+function updateCounterValue() {
+  valueEl.textContent = counterValue;
+}
