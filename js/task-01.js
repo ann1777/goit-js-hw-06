8e2dd5e0cba8be6b6c5765be423f325f11c9b869
Angela Potapchuk <angela.harkov@gmail.com>
2023-04-15 12:51:44 +0300

    task-01.js completed

--- js/task-01.js
+++ js/task-01.js
@@ -3,3 +3,28 @@
 // 2. Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст 
 // заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 
+const categories = document.querySelectorAll('ul#categories');
+
+const categoryItem = document.querySelectorAll('ul#categories > .item li');
+
+// 1. Counting and logs number of categories in ul#categories, i.e. li.item elements
+let categoryNum = 0;
+
+categories.forEach(category => {
+    let categoryNum = 0;
+    categoryNum += category.querySelectorAll('li.item').length;
+    console.log(`Number of categories: ${categoryNum}`); //Number of categories: 3.
+});
+
+// get all the categories
+const categoriType = document.querySelectorAll('.item');
+
+// loop through each category and count the number of <li> elements in their <ul>
+categoriType.forEach(category => {
+  const categoryName = category.querySelector('h2').textContent;
+  const count = category.querySelectorAll('ul li').length;
+
+  console.log(`  Category: ${categoryName}
+  Elements: ${count}`);
+});
+
